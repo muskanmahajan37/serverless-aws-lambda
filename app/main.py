5b574826b73a3462b8bdc7ee16d41da011b51ecb
vjanz <valon.januzaj98@gmail.com>
2020-11-07 02:15:01 +0100

    Initial commit

---
+++ app/main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+from mangum import Mangum
+
+from app.api.api_v1.api import router as api_router
+
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": f"This is our secret key value" }
+
+
+app.include_router(api_router)
+handler = Mangum(app)
